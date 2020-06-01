c544746278d25cc3ad1267060743315f3429b190
lennart.mischnaewski <lennart.mischnaewski@stud.tu-darmstadt.de>
2020-06-01 09:24:46 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='chainchat',
+    version='1.0',
+    description='chainchat is a simple Markov chain based way to generate chats from historical data.',
+    author='Lennart Mischnaewski',
+    author_email='lenmisch@gmail.com',
+    url='https://github.com/leny-mi/chainchat',
+    py_modules=['chainchat'],
+    install_requires=['scipy', 'numpy', 'six'],
+    dependency_links = [
+     "git+git://github.com/riccardoscalco/Pykov@master",
+    ],
+)
